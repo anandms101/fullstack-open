4b4dc3d552ee86bd3f382f72f473a541cad453fc
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-03-02 20:00:46 +0530

    part4 exc 4.1-4.2 working

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,49 +1,6 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = config.MONGO_URI;
-mongoose.connect(mongoUrl).then(() => {
-  logger.info('connected to MongoDB');
-}).catch((error) => {
-  logger.error('error connecting to MongoDB:', error.message);
-},
-);
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-      .find({})
-      .then((blogs) => {
-        response.json(blogs);
-      }).catch((error) => {
-        logger.error(error);
-      });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-      .save()
-      .then((result) => {
-        response.status(201).json(result);
-      });
-});
 
 const PORT = config.PORT || 3000;
 app.listen(PORT, () => {
